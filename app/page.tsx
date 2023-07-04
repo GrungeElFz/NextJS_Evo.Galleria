a1c5b85d57fd49cc69e6276f59f7a631f15d829e
GrungeElFz <elf0114_@hotmail.com>
2023-07-04 17:02:48 -0500

    Modified:   Replaced 'test' with `<HomeScreen />` component (app/page.tsx)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
+import React from "react";
+import HomeScreen from "@/components/HomeScreen";
+
 export default function Home() {
-  return (
-    <div className="font-2xl font-weight-bold">
-      <h1>Test</h1>
-    </div>
-  );
+  return <HomeScreen />;
 }
