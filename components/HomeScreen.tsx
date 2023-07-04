68c31f97d5ae1eb0e1025ba0ffc051b2c9b28622
GrungeElFz <elf0114_@hotmail.com>
2023-07-04 17:47:41 -0500

    Modified:   Added `Tesla Model 3` banner image (components/HomeScreen.tsx)

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -1,7 +1,14 @@
+import Image from "next/image";
+
 export default function HomeScreen() {
   return (
-    <div>
-      <h1>Home Screen</h1>
+    <div className="h-screen w-screen">
+      <Image
+        fill
+        src="/Banner-Tesla.Model.3.avif"
+        alt="Tesla Model 3"
+        style={{ objectFit: "cover" }}
+      />
     </div>
   );
 }
