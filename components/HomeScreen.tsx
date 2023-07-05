4968311ada651051c32f7f9f786a014e1e8bbc22
GrungeElFz <elf0114_@hotmail.com>
2023-07-04 19:58:58 -0500

    Refactored:   Add comments (components/HomeScreen.tsx)               Re-arranged lines

--- components/HomeScreen.tsx
+++ components/HomeScreen.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function HomeScreen() {
   return (
     <div className="h-screen w-screen">
+      {/* Background Image */}
       <Image
         fill
         src="/Banner-Tesla.Model.3.avif"
@@ -10,9 +11,13 @@ export default function HomeScreen() {
         style={{ objectFit: "cover" }}
         className="absolute z-0"
       />
+
+      {/* Model */}
       <h1 className="flex flex-col items-center justify-between text-center relative pt-48 text-black text-5xl font-normal">
         Model 3
       </h1>
+
+      {/* Text */}
       <p className="flex items-center justify-center text-center relative pt-5 text-black text-lg font-light">
         <a
           href="https://www.tesla.com/inventory/new/m3?arrangeby=relevance&zip=61801&range=200"
