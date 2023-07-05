bd92e7fd2da502847976ab5e0ef0404390ac3cb0
GrungeElFz <elf0114_@hotmail.com>
2023-07-05 17:31:44 -0500

    Refactored:   Removed unused component (components/HamburgerMenu.tsx)

--- components/HamburgerMenu.tsx
+++ components/HamburgerMenu.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { useRouter } from "next/navigation";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Check } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
