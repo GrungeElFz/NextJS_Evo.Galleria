302a25e989989bf9b711d5b3192cfb27222ed68e
GrungeElFz <elf0114_@hotmail.com>
2023-07-05 19:52:09 -0500

    Modified:   Temporary redirected to Tesla Model 3 route (app/page.tsx)

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
-import HomeScreen from "@/components/HomeScreen";
+import HomeScreen from "app/tesla/model-3/page.tsx";
 
 export default function Home() {
+  // Temporary redirect to `Tesla Model 3` Route
   return <HomeScreen />;
 }
